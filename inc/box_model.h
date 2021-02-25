c9c4317a5bc167fcf7d5e6732de1fc54821357bc
99003734 <78858575+99003734@users.noreply.github.com>
2021-02-25 14:41:20 +0530

    Update box_model.h

--- inc/box_model.h
+++ inc/box_model.h
@@ -9,7 +9,7 @@
 
 typedef struct
 { /* data */
-    int unique_id;
+    int user_id;
     int length;
     int breadth;
     int height;
@@ -21,6 +21,6 @@ typedef struct
 struct Boxes *head; 
 
 void display(const Boxes *ptr);
-Boxes *addbox(Boxes *box_add, int number_of_boxes);
+Boxes *addbox(Boxes *box_add, int no_of_boxes);
 
 #endif
