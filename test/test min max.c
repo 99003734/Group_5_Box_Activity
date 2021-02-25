d53e370f6ecd8180b1dd0eb0f59c3f2e268f9a0c
99003734 <78858575+99003734@users.noreply.github.com>
2021-02-25 14:52:04 +0530

    Update test min max.c

--- test/test min max.c
+++ test/test min max.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-      unsigned int number_of_boxes;
+      unsigned int no_of_boxes;
 
       printf("\nEnter number of boxes you want to add:");
-      scanf("%u",&number_of_boxes);
+      scanf("%u",&no_of_boxes);
       
-      Box *box1=(Box*)calloc(number_of_boxes,sizeof(Box));
+      Box *box1=(Box*)calloc(no_of_boxes,sizeof(Box));
      
       box1= add_boxes(box1,number_of_boxes);
 
       
-      unsigned long diff=difference_min_max_volume(box1,number_of_boxes);
+      unsigned long diff=difference_min_max_volume(box1,no_of_boxes);
 
       printf("\nDifference between maximum and minimum volume : ");
       printf("%lu",diff);
