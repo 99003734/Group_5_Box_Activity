6633624dcb71d48623848ad0512d81437dd7e9fb
99003734 <78858575+99003734@users.noreply.github.com>
2021-02-25 14:44:50 +0530

    Update volume.h

--- inc/volume.h
+++ inc/volume.h
@@ -4,7 +4,7 @@
 #include"boxmodel.h"
 
 unsigned long volume_of_box(Boxes *box1);
-unsigned long avg_volume(Boxes *box1, unsigned int number_of_boxes);
+unsigned long avg_volume(Boxes *box1, unsigned int no_of_boxes);
 
 
 
