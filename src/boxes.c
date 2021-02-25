1cec8f08afeaa04785abdd84d33166e83d1b321e
99003734 <78858575+99003734@users.noreply.github.com>
2021-02-25 14:47:35 +0530

    Update boxes.c

--- src/boxes.c
+++ src/boxes.c
@@ -3,9 +3,9 @@
 #include<stdlib.h>
 #include<string.h>
 
-Box* add_boxes(Box *box1,unsigned int number_of_boxes)
+Box* add_boxes(Box *box1,unsigned int no_of_boxes)
 {
-    unsigned int unique_id1;
+    unsigned int user_id1;
     unsigned int length1;
     unsigned int breadth1;
     unsigned int height1;
@@ -17,10 +17,10 @@ Box* add_boxes(Box *box1,unsigned int number_of_boxes)
         printf("You can't add anymore boxes");
         return 0;
     }
-    for(int i=0;i<number_of_boxes;i++)
+    for(int i=0;i<no_of_boxes;i++)
     {
         printf("\nEnter Unique ID for the BOX :");
-        scanf("%u",&unique_id1);
+        scanf("%u",&user_id1);
 
          printf("\nEnter Length for the BOX : ");
         scanf("%u",&length1);
@@ -37,7 +37,7 @@ Box* add_boxes(Box *box1,unsigned int number_of_boxes)
         printf("\nEnter Weight for the BOX : ");
         scanf("%u",&weight1);
 
-        box1->unique_id=unique_id1;
+        box1->user_id=user_id1;
         box1->length=length1;
         box1->breadth=breadth1;
         box1->height=height1;
